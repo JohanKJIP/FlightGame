ba76b6fcf4f90021513d5f69276b6b6ec7f98303
Johan von Hacht <johvh@kth.se>
2019-07-01 10:33:32 +0200

    added checkpoints

---
+++ Assets/Scripts/AirplaneStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneStats : MonoBehaviour
+{
+    public GameObject[] checkPoints;
+    private int currentCheckpoint;
+    
+    void Start()
+    {
+        currentCheckpoint = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "checkpoint")
+        {
+            // checkpoints left
+            if (currentCheckpoint + 1 < checkPoints.Length)
+            {
+                checkPoints[currentCheckpoint + 1].SetActive(true);
+                checkPoints[currentCheckpoint].SetActive(false);
+            }
+            currentCheckpoint++;
+
+            // end of course/checkpoints
+            if (currentCheckpoint == checkPoints.Length)
+            {
+                // TODO END game
+                checkPoints[checkPoints.Length-1].SetActive(false);
+                Debug.Log("END OF GAME!!!");
+            }
+            Debug.Log(currentCheckpoint);
+        }
+    }
+}
